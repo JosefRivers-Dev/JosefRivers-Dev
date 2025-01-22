9dfbb2fec06291e2e0defa368f13eacc205a75b0
JosefRivers-Dev <jovannyjosef_rivera@outlook.com>
2025-01-22 10:49:45 -0600

    Add files via upload

---
+++ Python/tiposdeerrores.py
@@ -0,0 +1,21 @@
+# ERRORES DE SINTAXIS
+
+# var = imput("Ingrese algo:")
+
+# if variable == 2:
+#     print("Da error")
+
+# while True
+#     print("Hola")
+
+# ERRORES DE EJECUCIÓN
+
+# var = int(input("Ingresa un numero:"))
+
+# var = 2/0
+
+# ERRORES LOGICOS
+
+# if 3 < 0:
+#     print("Hola")
+# print("como estas?")
